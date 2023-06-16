6f9e79e4c755919f65f3ec5a6f078656a6e78566
OnkarTriwits <114130214+OnkarTriwits@users.noreply.github.com>
2023-06-16 12:10:11 +0530

    nothing

--- application/views/FetchView.php
+++ application/views/FetchView.php
@@ -4,30 +4,70 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Data</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            padding: 20px;
+        }
+
+        h2 {
+            color: #333;
+            text-align: center;
+        }
+
+        .container {
+            display: flex;
+            justify-content: center;
+        }
+
+        table {
+            border-collapse: collapse;
+            margin-top: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        table td,
+        table th {
+            padding: 10px;
+            border: 1px solid #ccc;
+        }
+
+        table th {
+            background-color: #f0f0f0;
+            text-align: left;
+        }
+    </style>
 </head>
 <body>
-    <h2>User Profile</h2>
-    <table>
-        <tr>
-            <td>Name:</td>
-            <td><?php echo $this->session->userdata('user')['name']; ?></td>
-        </tr>
-        <tr>
-            <td>Email:</td>
-            <td><?php echo $this->session->userdata('user')['email']; ?></td>
-        </tr>
-        <tr>
-            <td>Phone:</td>
-            <td><?php echo $this->session->userdata('user')['phone']; ?></td>
-        </tr>
-        <tr>
-            <td>Password:</td>
-            <td><?php echo $this->session->userdata('user')['password']; ?></td>
-        </tr>
-        <tr>
-            <td>File:</td>
-            <td><?php echo $this->session->userdata('user')['file_path']; ?></td>
-        </tr>
-    </table>
+    <div class="container">
+        <h2>User Profile</h2>
+    </div>
+    <div class="container">
+        <table>
+            <tr>
+                <th>Name</th>
+                <td><?php echo $this->session->userdata('user')['name']; ?></td>
+            </tr>
+            <tr>
+                <th>Email</th>
+                <td><?php echo $this->session->userdata('user')['email']; ?></td>
+            </tr>
+            <tr>
+                <th>Phone</th>
+                <td><?php echo $this->session->userdata('user')['phone']; ?></td>
+            </tr>
+            <tr>
+                <th>Password</th>
+                <td><?php echo $this->session->userdata('user')['password']; ?></td>
+            </tr>
+            <tr>
+                <th>File</th>
+                <td><?php echo $this->session->userdata('user')['file_path']; ?></td>
+            </tr>
+        </table>
+    </div>
 </body>
 </html>
