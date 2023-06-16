d275255dca5775f8a6918fbdf443706fc0e86cfe
OnkarTriwits <114130214+OnkarTriwits@users.noreply.github.com>
2023-06-16 15:50:35 +0530

    now done

--- application/models/AllUsersModel.php
+++ application/models/AllUsersModel.php
@@ -2,6 +2,7 @@
 class AllUsersModel extends CI_Model{
     public function getAllUsers(){
         $query = $this->db->get('user');
+        // $query = $this->db->order_by('slno', 'desc')->get('user');
         return $query->result_array();
     }
 } 
