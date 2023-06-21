5a35c34f55a42ecbed4924d3d9bcaa706459a7d3
OnkarTriwits <114130214+OnkarTriwits@users.noreply.github.com>
2023-06-21 12:01:05 +0530

    made some changes

--- application/controllers/RegController.php
+++ application/controllers/RegController.php
@@ -55,43 +55,41 @@ class RegController extends CI_Controller {
 
 
 <?php
-// class RegController extends CI_Controller {
-//     public function index() {
-//         $data['message'] = ''; // Initialize the message variable
+class RegController extends CI_Controller {
+    public function index() {
+        $data['message'] = ''; // Initialize the message variable
 
-//         if ($this->input->post('register')) {
-//             $name = $this->input->post('name');
-//             $email = $this->input->post('email');
-//             $phone = $this->input->post('phone');
-//             $password = $this->input->post('password');
+        if ($this->input->post('register')) {
+            $name = $this->input->post('name');
+            $email = $this->input->post('email');
+            $phone = $this->input->post('phone');
+            $password = $this->input->post('password');
 
-//             // File Upload
-//             $config['upload_path'] = './uploads/';
-//             $config['allowed_types'] = 'gif|jpg|jpeg|png';
-//             $this->load->library('upload', $config);
+            // File Upload
+            $config['upload_path'] = './uploads/';
+            $config['allowed_types'] = 'gif|jpg|jpeg|png';
+            $this->load->library('upload', $config);
 
-//             if (!$this->upload->do_upload('file')) {
-//                 $data['message'] = $this->upload->display_errors('<h3 style="color:red">', '</h3>');
-//             } else {
-//                 // File uploaded successfully
-//                 $fileData = $this->upload->data();
-//                 $filePath = $fileData['file_name']; 
+            if (!$this->upload->do_upload('file')) {
+                $data['message'] = $this->upload->display_errors('<h3 style="color:red">', '</h3>');
+            } else {
+                // File uploaded successfully
+                $fileData = $this->upload->data();
+                $filePath = $fileData['file_name']; 
 
-//                 // Continue with database insertion and other logic
+                // Continue with database insertion and other logic
+                $this->load->model('RegModel');
+                $data = $this->RegModel->insert_data($name, $email, $phone, $password, $filePath);
 
-//                 // Example database insertion
-//                 $this->load->model('RegModel');
-//                 $data = $this->RegModel->insert_data($name, $email, $phone, $password, $filePath);
+                $data['message'] = $data['message']; // Update the message from the model
+            }
+        }
 
-//                 $data['message'] = $data['message']; // Update the message from the model
-//             }
-//         }
-
-//         $this->load->view('RegView', $data); // Pass the $data variable to the view
-//     }
+        $this->load->view('RegView', $data); // Pass the $data variable to the view
+    }
 
  
-// }
+}
 
 ?>
 
--- application/models/RegModel.php
+++ application/models/RegModel.php
@@ -34,22 +34,22 @@ class RegModel extends CI_Model {
 
 
 <?php
-// class RegModel extends CI_Model {
-//     function insert_data($name, $email, $phone, $password, $filePath) {
-//         $query = $this->db->query("SELECT * FROM user WHERE (email = '".$email."' OR phone = '".$phone."')");
-//         $row = $query->num_rows();
+class RegModel extends CI_Model {
+    function insert_data($name, $email, $phone, $password, $filePath) {
+        $query = $this->db->query("SELECT * FROM user WHERE (email = '".$email."' OR phone = '".$phone."')");
+        $row = $query->num_rows();
 
-//         if ($row) {
-//             $data['message'] = "<h3 style='color:red'>This user is already registered</h3>";
-//         } else {
-//             $query = $this->db->query("INSERT INTO user SET name = '$name', email = '$email', phone = '$phone', password = '$password', file_path = '$filePath'");
+        if ($row) {
+            $data['message'] = "<h3 style='color:red'>This user is already registered</h3>";
+        } else {
+            $query = $this->db->query("INSERT INTO user SET name = '$name', email = '$email', phone = '$phone', password = '$password', file_path = '$filePath'");
 
-//             $data['message'] = "<h3 style='color:blue'>You are registered successfully</h3>";
-//         }
+            $data['message'] = "<h3 style='color:blue'>You are registered successfully</h3>";
+        }
 
-//         return $data; // Return the data instead of loading the view
-//     }
-// }
+        return $data; // Return the data instead of loading the view
+    }
+}
 
 
 ?>
